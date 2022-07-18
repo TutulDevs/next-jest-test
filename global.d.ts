14745d8330f731068157de3855ed8c21f898f637
tutul <tutuldevs.jobs@gmail.com>
2022-07-18 18:55:42 +0600

    ui test done

---
+++ global.d.ts
@@ -0,0 +1,33 @@
+export {};
+
+declare global {
+  interface Geo {
+    lat: string;
+    lng: string;
+  }
+
+  interface Address {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: Geo;
+  }
+
+  interface Company {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  }
+
+  interface HomeData {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: Address;
+    phone: string;
+    website: string;
+    company: Company;
+  }
+}
---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+};
+
+module.exports = createJestConfig(customJestConfig);
